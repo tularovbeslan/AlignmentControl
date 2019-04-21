986d6f747f18633478e7e75b4a3eb2d2a85a0883
Beslan Tularov <tularovbeslan@gmail.com>
2019-04-21 22:42:07 +0300

    update example project

--- Example/AlignmentControl/ViewController.swift
+++ Example/AlignmentControl/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
 	@IBOutlet weak var contentView: UIView!
 	@IBOutlet weak var redView: UIView!
 
+	var axisX: CGFloat = 0
+	var axisY: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,10 +29,17 @@ class ViewController: UIViewController {
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 
+ 		contentViewSetup()
 
+		redViewSetup()
+
+
+	}
+
+	fileprivate func contentViewSetup() {
 
 		let lineDash = CAShapeLayer()
-		lineDash.strokeColor = UIColor.lightText.cgColor
+		lineDash.strokeColor = UIColor.black.cgColor
 		lineDash.lineDashPattern = [3, 3]
 		lineDash.frame = contentView.bounds
 		lineDash.fillColor = nil
@@ -39,14 +49,56 @@ class ViewController: UIViewController {
 		contentView.layer.cornerRadius = 30
 		contentView.clipsToBounds = true
 		contentView.layer.addSublayer(lineDash)
+	}
 
+	fileprivate func redViewSetup() {
+
+		redView.layer.cornerRadius = 30
+		redView.clipsToBounds = true
+		redView.frame = CGRect(x: 0,
+							   y: 0,
+							   width: contentView.frame.width / 3,
+							   height: contentView.frame.height / 3)
 	}
 }
 
 extension ViewController: AlingmentViewDelegate {
     
 	func didSelectOptionFor(_ aligment: AlignmentMode) {
-		print(String(describing: aligment))
+		switch aligment {
+		case .Left:
+			axisX = 0
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		case .Center:
+			axisX = (self.contentView.frame.width / 2) -  (self.redView.frame.width / 2)
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		case .Right:
+			axisX = self.contentView.frame.width - self.redView.frame.width
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		case .Top:
+			axisY = 0
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		case .Middle:
+			axisY = (self.contentView.frame.height / 2) -  (self.redView.frame.height / 2)
+
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		case .Bottom:
+			axisY = self.contentView.frame.height - self.redView.frame.height
+
+			UIView.animate(withDuration: 0.3) {
+				self.redView.transform = CGAffineTransform(translationX: self.axisX, y: self.axisY)
+			}
+		}
 	}
 }
 
